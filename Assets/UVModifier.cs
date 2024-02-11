d5078e8f400f50284ef948ca98796a824b480a87
Brett Bass <bbass@ucsd.edu>
2024-02-10 20:10:13 -0800

    switched to tuple swap

--- Assets/UVModifier.cs
+++ Assets/UVModifier.cs
@@ -13,23 +13,12 @@ public class UVModifier : MonoBehaviour
 
 
         // back face
-        var tmp1 = uvs[6];
-        var tmp2 = uvs[7];
-
-        uvs[6] = uvs[11];
-        uvs[7] = uvs[10];
-        uvs[10] = tmp2;
-        uvs[11] = tmp1;
-
+        (uvs[6], uvs[11]) = (uvs[11], uvs[6]);
+        (uvs[7], uvs[10]) = (uvs[10], uvs[7]);
 
         // bottom face
-        tmp1 = uvs[15];
-        tmp2 = uvs[12];
-
-        uvs[15] = uvs[13];
-        uvs[13] = tmp1;
-        uvs[12] = uvs[14];
-        uvs[14] = tmp2;
+        (uvs[15], uvs[13]) = (uvs[13], uvs[15]);
+        (uvs[12], uvs[14]) = (uvs[14], uvs[12]);
 
         // Apply the modified UVs back to the mesh
         mesh.uv = uvs;
